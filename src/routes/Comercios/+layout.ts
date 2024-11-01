68abb8864438204e4529c1e1e101f195b885030f
Nandosrv <nandooliveirasrv8@gmail.com>
2024-11-01 01:11:46 -0300

    fix:remove routes img

--- src/routes/Comercios/+layout.ts
+++ src/routes/Comercios/+layout.ts
@@ -1,3 +1,12 @@
+
+	import Americana from '$lib/images/th.jpeg';
+	import lorm from '$lib/images/th1.jpeg';
+
+
+
+
+
+
 export function load() {
     return {
         categories:[
@@ -10,14 +19,14 @@ export function load() {
             
         ],
         items: [
-            {slug:  'farmcia-Advogados', title: 'TagLiarini & Capeletti Advogados', category: 'Farmacia',telefone:'(16) 3954-200221', hours: "Seg-Sex: 08h às 18h", image: '/src/lib/images/th1.jpeg' },
+            {slug:  'farmcia-Advogados', title: 'TagLiarini & Capeletti Advogados', category: 'Farmacia',telefone:'(16) 3954-200221', hours: "Seg-Sex: 08h às 18h", image: Americana },
             { slug:  'Loja Virtuais', title: 'Loja da Dinda', category: 'Loja Virtuais',telefone:'(16) 3954-2001',hours: "Seg-Sex: 08h às 18h", image: 'https://placehold.co/600x400/EEE/31343C' },
             { slug:  'Beautify Blonde Hair', title: 'Beautify Blonde Hair', category: 'Websites',telefone:'(16) 3954-2001',hours: "Seg-Sex: 08h às 18h", image: 'https://placehold.co/600x400/EEE/31343C' },
             { slug: 'Lu e Lulu na Pomerânia', title: 'Lu e Lulu na Pomerânia', category: 'Blogs',telefone:'(16) 3954-2001',hours: "Seg-Sex: 08h às 18h", image: 'https://placehold.co/600x400/EEE/31343C' },
             { slug: 'Receitas Macarrônicas', title: 'Receitas Macarrônicas', category: 'Blogs',telefone:'(16) 3954-2001',hours: "Seg-Sex: 08h às 18h", image: 'https://placehold.co/600x400/EEE/31343C' },
             { slug: 'Receitas Macarrônicas', title: 'Receitas Macarrônicas', category: 'Blogss',telefone:'(16) 3954-2001',hours: "Seg-Sex: 08h às 18h", image: 'https://placehold.co/600x400/EEE/31343C' },
             {slug: 'Receitas Macarrônicas', title: 'Receitas Macarrônicas', category: 'Blogss',telefone:'(16) 3954-2001',hours: "Seg-Sex: 08h às 18h", image: 'https://placehold.co/600x400/EEE/31343C' },
-            { slug: 'farmacia-do-joao', title: 'farmacia do joao', category: 'Farmacia', telefone:'(16) 3954-2001',hours: "Seg-Sex: 08h às 18h", image: '/src/lib/images/th.jpeg' },
+            { slug: 'farmacia-do-joao', title: 'farmacia do joao', category: 'Farmacia', telefone:'(16) 3954-2001',hours: "Seg-Sex: 08h às 18h", image: lorm },
             { slug: 'Receitas-Macarrônicas', title: 'Receitas Macarrônicas', category: 'Blogss',telefone:'(16) 3954-2001',hours: "Seg-Sex: 08h às 18h", image: 'https://placehold.co/600x400/EEE/31343C' },
             { slug: 'Receitas Macarrônicas', title: 'Receitas Macarrônicas', category: 'Blogss',telefone:'(16) 3954-2001',hours: "Seg-Sex: 08h às 18h", image: 'https://placehold.co/600x400/EEE/31343C' },
             { slug: 'text-Macarrônicas', title: 'Receitas Macarrônicas', category: 'Blogss',telefone:'(16) 3954-2001',hours: "Seg-Sex: 08h às 18h", image: 'https://placehold.co/600x400/EEE/31343C' },
@@ -26,6 +35,7 @@ export function load() {
 
         ]
     }
+    
 }
 
 
