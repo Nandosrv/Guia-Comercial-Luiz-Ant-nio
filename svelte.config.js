73a6f29dfe7fac0ee2b6d9462b4b16150d290cc1
Nandosrv <nandooliveirasrv8@gmail.com>
2024-11-08 15:33:54 -0300

    codigo certo pra funcionar

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,18 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterCloudflare from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import 'dotenv/config'
+
+const adapters = {
+	'cloudflare': adapterCloudflare,
+	'default': adapterAuto
+}
+
+function getAdapter(){
+	let adpt = process.env.VITE_ADAPTER ?? 'default'
+	if (!(adpt in adapters)) adpt = 'default'
+	return adapters[adpt]()
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +24,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: getAdapter()
 	}
 };
 
