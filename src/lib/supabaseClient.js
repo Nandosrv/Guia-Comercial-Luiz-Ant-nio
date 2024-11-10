1cc66f5be8d3502523c7600415c746f201372d6a
Nandosrv <nandooliveirasrv8@gmail.com>
2024-11-10 02:33:28 -0300

    partf

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Substitua pelas suas credenciais do Supabase
-const supabaseUrl = 'https://bgckerfaimtiavllgvuo.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJnY2tlcmZhaW10aWF2bGxndnVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA2ODA1MDgsImV4cCI6MjA0NjI1NjUwOH0.rmQ8a-19BZ8gpB_Qysyu_3kvO7Wp-lwsYflyuzWSF6w';
+const supabaseUrl = 'https://rxkrvqhmfbjbxhwukdct.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4a3J2cWhtZmJqYnhod3VrZGN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyMTU3ODIsImV4cCI6MjA0Njc5MTc4Mn0.ddzMg_3ZpyNEGYKQzRt-TUv0pND33ABedKaZeH50PJA';
 
-// Criação do cliente Supabase
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
