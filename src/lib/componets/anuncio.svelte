5406d33463b20421756c4c8cb41022781bd89573
Nandosrv <nandooliveirasrv8@gmail.com>
2024-12-02 23:50:16 -0300

    arrumando arquivos do anuncio

--- src/lib/componets/anuncio.svelte
+++ src/lib/componets/anuncio.svelte
@@ -1,39 +1,33 @@
-<main class="flex h-[500px] w-[100%] items-center justify-center">
-	<div class="flex h-[300px] w-[95%] flex-col items-center justify-center">
-		<div class=" ml-1 flex h-[30px] w-[100%] items-center justify-center gap-2">
-			<div class="flex h-[30px] w-[92%] items-center justify-start gap-2">
-				<p class="font-serif text-white">NOTÍCIAS</p>
-				<p class="font-serif text-blue-600">PRINCIPAIS</p>
+<main class="w-[100%] h-[500px] flex justify-center items-center ">
+	<div class="w-[95%] h-[300px] flex flex-col justify-center items-center">
+		<div class=" ml-1 w-[100%] h-[30px] flex justify-center items-center  gap-2">
+			<div class="w-[92%] h-[30px] flex justify-start items-center  gap-2">
+				<p class="text-white font-serif">NOTÍCIAS</p>
+				 <p class="text-blue-600 font-serif">PRINCIPAIS</p>
 			</div>
 		</div>
-		<div
-			class="flex h-[95%] w-[90%] flex-col items-center justify-center gap-2 border border-gray-700"
-		>
-			<div class="flex h-[100px] w-[100%] flex-col border-l-4 border-red-500">
-				<a href="/Comercios">
-					<p class="font-serif text-blue-600">Manutenção Programada (05/12/2024)</p>
-				</a>
-				<p class="font-serif text-white">05/12/2024</p>
-				<span class="font-serif text-white"
-					>No dia 20/11/2024 às 13:00 horas, realizamos uma manutenção programada.</span
-				>
-			</div>
-			<div class="h-[100px] w-[100%] border-l-4 border-green-600">
-				<a href="/Avalie">
-					<p class="font-serif text-blue-600">Manutenção Em Andamento</p>
-				</a>
-				<p class="font-serif text-white">13/11/2024</p>
-				<p class="font-serif text-white">
-					Não encontrou seu comércio no nosso site?" Entre em contato com a nossa equipe!"
-				</p>
-			</div>
-			<div class="h-[100px] w-[100%] border-l-4 border-green-600">
-				<a href="/Avalie">
-					<p class="font-serif text-blue-600">Avalie nosso site!</p>
-				</a>
-				<p class="font-serif text-white">13/11/2024</p>
-				<p class="text-white">Sua opinião é muito importante para nós</p>
-			</div>
-		</div>
+		<div class="w-[90%] h-[95%] flex flex-col justify-center items-center border border-gray-700 gap-2">
+		   <div class="w-[100%] h-[100px] flex flex-col border-l-4 border-red-500">
+			<a href="/Comercios">
+				<p class="text-blue-600 font-serif">Manutenção Programada (05/12/2024)</p>
+			</a>
+			<p class="text-white font-serif">05/12/2024</p>
+			<span class="text-white font-serif">No dia 20/11/2024 às 13:00 horas, realizamos uma manutenção programada.</span>
+		   </div>
+		   <div class="w-[100%] h-[100px]  border-l-4 border-green-600">
+			<a href="/Avalie">
+				<p class="text-blue-600 font-serif">Manutenção Em Andamento</p>
+			</a>
+			<p class="text-white font-serif">13/11/2024</p>
+			<p class="text-white font-serif">Não encontrou seu comércio no nosso site?" Entre em contato com a nossa equipe!"</p>
+		   </div>
+		   <div class="w-[100%] h-[100px]  border-l-4 border-green-600">
+			<a href="/Avalie">
+				<p class="text-blue-600 font-serif">Avalie nosso site!</p>
+			</a>
+			<p class="text-white font-serif">13/11/2024</p> 
+			<p class="text-white"> Sua opinião é muito importante para nós </p>
+		   </div>
+		 </div>
 	</div>
 </main>
