0ad64646e3a24212b01efefdba1032e0c99d1b6b
Paulo Soares <pbss2007@gmail.com>
2024-12-08 14:10:00 -0300

    fix: aplicando chaves privadas ( firebase / supabase )

--- src/firebase.js
+++ src/firebase.js
@@ -1,35 +1,45 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import {
-  getAuth,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  GoogleAuthProvider,
-  signInWithPopup,
-  setPersistence,
-  browserSessionPersistence,
-  browserLocalPersistence,
-} from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+	getAuth,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
+	GoogleAuthProvider,
+	signInWithPopup,
+	setPersistence,
+	browserSessionPersistence,
+	browserLocalPersistence
+} from 'firebase/auth';
+import { getAnalytics } from 'firebase/analytics';
+
+import {
+	PUBLIC_FIREBASE_API_KEY,
+	PUBLIC_FIREBASE_AUTH_DOMAIN,
+	PUBLIC_FIREBASE_PROJECT_ID,
+	PUBLIC_FIREBASE_STORAGE_BUCKET,
+	PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	PUBLIC_FIREBASE_APP_ID,
+	PUBLIC_FIREBASE_MEASUREMENT_ID
+} from '$env/static/public';
 
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBxt3-VD2D2_f8MXhLCvPNFHmUCk83WcXE",
-  authDomain: "loginflow-5132e.firebaseapp.com",
-  projectId: "loginflow-5132e",
-  storageBucket: "loginflow-5132e.firebasestorage.app",
-  messagingSenderId: "971812191713",
-  appId: "1:971812191713:web:0c6981932e1dc3266aa6a0",
-  measurementId: "G-W5MRMQDDSL",
+	apiKey: PUBLIC_FIREBASE_API_KEY,
+	authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: PUBLIC_FIREBASE_APP_ID,
+	measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Agora você pode usar o Firebase, por exemplo:
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
- 
+
 // Inicializa o Analytics apenas se estiver no lado do cliente
 let analytics;
-if (typeof window !== "undefined") {
-  analytics = getAnalytics(app); // Somente inicializar no cliente
+if (typeof window !== 'undefined') {
+	analytics = getAnalytics(app); // Somente inicializar no cliente
 }
 
 // Obtém a instância de autenticação do Firebase
@@ -37,21 +47,21 @@ const auth = getAuth(app);
 
 // Define o tipo de persistência da sessão
 setPersistence(auth, browserLocalPersistence) // Use browserSessionPersistence se preferir sessão temporária
-  .then(() => {
-    console.log("Persistência de sessão configurada com sucesso.");
-  })
-  .catch((error) => {
-    console.error("Erro ao configurar persistência de sessão:", error);
-  });
+	.then(() => {
+		console.log('Persistência de sessão configurada com sucesso.');
+	})
+	.catch((error) => {
+		console.error('Erro ao configurar persistência de sessão:', error);
+	});
 
 // Google Auth Provider
 const googleProvider = new GoogleAuthProvider();
 
 export {
-  auth,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  googleProvider,
-  signInWithPopup,
+	auth,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
+	googleProvider,
+	signInWithPopup
 };
 export { app as firebaseApp, analytics };
--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,6 @@
-
+import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wsxmoaqtqsncoaawmuhp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzeG1vYXF0cXNuY29hYXdtdWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIzMDU0NjgsImV4cCI6MjA0Nzg4MTQ2OH0.cL3pGYarT9LhhZZ3JG10lpXyIy8Om_r2T14rCr8xrh4'; 
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY);
 
 export default supabase;
