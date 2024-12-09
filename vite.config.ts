ed1c9b0e16994eac92692ce0a7c0ad45f512107b
Nandosrv <nandooliveirasrv8@gmail.com>
2024-12-09 03:58:42 -0300

    feat aplicando dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
+import dotenv from 'dotenv';
+dotenv.config();
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		// Exponha apenas as variáveis públicas prefixadas com "PUBLIC"
+		'process.env': {
+		  ...Object.fromEntries(
+			Object.entries(process.env).filter(([key]) => key.startsWith('PUBLIC'))
+		  )
+		}
+	  },
 });
