902bee3e072155206562e9522fe9f62f65acb12b
Nandosrv <nandooliveirasrv8@gmail.com>
2024-12-09 12:43:49 -0300

    fix: supabseClient

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,8 @@
-import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL  } from '$env/static/public';
+// import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL  } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
+
+const PUBLIC_SUPABASE_KEY = process.env.PUBLIC_SUPABASE_KEY;
+const PUBLIC_SUPABASE_URL = process.env.PUBLIC_SUPABASE_URL;
 
 const supabase = createClient(PUBLIC_SUPABASE_URL,PUBLIC_SUPABASE_KEY);
 
