649ea7efdbacbcceb7b7e579a9282c17ed606e6b
Nandosrv <nandooliveirasrv8@gmail.com>
2024-12-09 13:22:13 -0300

    fix: define env no vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	define: {
 		'process.env.PUBLIC_SUPABASE_URL': JSON.stringify(process.env.PUBLIC_SUPABASE_URL),
-		'process.env.PUBLIC_SUPABASE_KEY': JSON.stringify(process.env.PUBLIC_SUPABASE_KEY)
+		'process.env.PUBLIC_SUPABASE_KEY': JSON.stringify(process.env.PUBLIC_SUPABASE_KEY),
+		'process.env.PUBLIC_FIREBASE_API_KEY': JSON.stringify(process.env.PUBLIC_FIREBASE_API_KEY),
+		'process.env.PUBLIC_FIREBASE_APP_ID': JSON.stringify(process.env.PUBLIC_FIREBASE_APP_ID),
+		'process.env.PUBLIC_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.PUBLIC_FIREBASE_AUTH_DOMAIN),
+		'process.env.PUBLIC_FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.PUBLIC_FIREBASE_MEASUREMENT_ID),
+		'process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+		'process.env.PUBLIC_FIREBASE_PROJECT_ID': JSON.stringify(process.env.PUBLIC_FIREBASE_PROJECT_ID),
+		'process.env.PUBLIC_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.PUBLIC_FIREBASE_STORAGE_BUCKET),
 	  }
 });
