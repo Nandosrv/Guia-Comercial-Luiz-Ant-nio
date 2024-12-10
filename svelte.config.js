0e5a8077b9fa7cf9dc613a1ef4f6e67cce59b71b
Nandosrv <nandooliveirasrv8@gmail.com>
2024-12-09 22:01:49 -0300

    fix: adapterCloudflare comentando

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,16 @@ import adapterCloudflare from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import 'dotenv/config'
 
-const adapters = {
-	'cloudflare': adapterCloudflare,
-	'default': adapterAuto
-}
+// const adapters = {
+// 	'cloudflare': adapterCloudflare,
+// 	'default': adapterAuto
+// }
 
-function getAdapter(){
-	let adpt = process.env.VITE_ADAPTER ?? 'default'
-	if (!(adpt in adapters)) adpt = 'default'
-	return adapters[adpt]()
-}
+// function getAdapter(){
+// 	let adpt = process.env.VITE_ADAPTER ?? 'default'
+// 	if (!(adpt in adapters)) adpt = 'default'
+// 	return adapters[adpt]()
+// }
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -24,7 +24,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: getAdapter(),
+		adapter: adapterCloudflare(),
 		paths: {
             base: '', // Defina se seu app não está na raiz
         },
