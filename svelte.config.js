e9867d23eb6b676d0a309ea6641a9dc952f4412e
Nandosrv <nandooliveirasrv8@gmail.com>
2024-12-09 22:13:13 -0300

    fix: agora vai confia

--- svelte.config.js
+++ svelte.config.js
@@ -25,9 +25,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapterCloudflare(),
-		paths: {
-            base: '/.svelte-kit/cloudflare', // Defina se seu app não está na raiz
-        },
+	
 		env: {
 			publicPrefix: 'PUBLIC_',
 			privatePrefix:'PRIVATE_'
