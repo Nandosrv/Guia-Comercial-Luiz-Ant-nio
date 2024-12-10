288eebcbc3b0b43e4e3c793d002d482f4794c431
Nandosrv <nandooliveirasrv8@gmail.com>
2024-12-09 21:52:18 -0300

    fix: paths

--- svelte.config.js
+++ svelte.config.js
@@ -25,6 +25,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: getAdapter(),
+		paths: {
+            base: '/.svelte-kit/cloudflare', // Defina se seu app não está na raiz
+        },
 		env: {
 			publicPrefix: 'PUBLIC_',
 			privatePrefix:'PRIVATE_'
