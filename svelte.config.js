3a3ccdcd296c8342ea48b911d894dc60ec3f86ba
Nandosrv <nandooliveirasrv8@gmail.com>
2024-12-09 22:32:50 -0300

    fix: deixando apenas o routes

--- svelte.config.js
+++ svelte.config.js
@@ -29,12 +29,6 @@ const config = {
 				include: ['/*'],
 				exclude: ['<all>']
 			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			},
 		}),
 		
 		// paths: {
