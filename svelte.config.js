9b4a308df8d2050a20b0c8aafceef8bcf10add46
Nandosrv <nandooliveirasrv8@gmail.com>
2024-12-09 22:24:57 -0300

    fix: routes adapter

--- svelte.config.js
+++ svelte.config.js
@@ -24,7 +24,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapterAuto(),
+		adapter: adapterCloudflare(),
+		routes: {
+			include: ['/*'],
+			exclude: ['<all>']
+		},
 		// paths: {
         //     base: '/.svelte-kit/cloudflare', // Defina se seu app não está na raiz
         // },
